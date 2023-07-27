26211dc4d6e0cad496b23d084198674ebc5a7e8b
Gustavo Silva <guh_sz@outlook.com>
2023-07-27 16:56:07 -0300

    ignore

---
+++ exercícios/chatbot.js
@@ -0,0 +1,52 @@
+
+
+function handleInput() {
+
+  // Get the user input.
+  var input = $("#chatbox").val();
+
+  // Send the user input to the server.
+  $.ajax({
+      url: "/chat",
+      data: { input: input },
+      success: function(response) {
+
+          // Get the response from the server.
+          var responseText = response.text;
+
+          // Display the response in the chatbox.
+          $("#chatbox").val(responseText);
+      }
+  });
+}
+
+function fetchResponse() {
+
+  // Get the user input.
+  var input = $("#chatbox").val();
+
+  // Send the user input to the server.
+  $.ajax({
+      url: "/chat",
+      data: { input: input },
+      success: function(response) {
+
+          // Get the response from the server.
+          var responseText = response.text;
+
+          // Display the response in the chatbox.
+          $("#chatbox").val(responseText);
+      }
+  });
+}
+
+$("#chatbox").on("input", handleInput);
+
+$(document).ready(function() {
+  fetchResponse();
+});
+
+from flask import Flask, request
+
+app = Flask(__name__)
+
