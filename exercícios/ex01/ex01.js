92fbe31ae1a2fbaa5cefeb6ee38bf343e8e9902a
Gustavo Silva <guh_sz@outlook.com>
2023-07-29 23:14:34 -0300

    terminei o desafiooo

--- exercícios/ex01/ex01.js
+++ exercícios/ex01/ex01.js
@@ -1,4 +1,21 @@
 var agora=new Date()
 var now=agora.getHours()
-var a = document.getElementById("hora")
-a.innerHTML=`<p>Agora são exatamrntr ${now} horas</p>`
+var a = document.getElementById("agr")
+var b = document.getElementById("hora")
+var c =document.getElementById("tenha")
+
+if (now<18){
+    a.innerHTML=`Agora são exatamente${now} horas`
+    b.innerHTML=`<img src="img/Amanhecer01.jpg" alt="">`
+    c.innerHTML=`Tenha uma boa tarde`
+} else if (now<24) {
+    a.innerHTML=`Agora são exatamente${now} horas`
+    b.innerHTML=`<img src="img/<img src="img/images.jpeg" alt="">" alt="">`
+    c.innerHTML=`Tenha uma boa noite`
+} else if (now<12){
+    a.innerHTML=`Agora são exatamente${now} horas`
+    b.innerHTML=`<img src="img/pexels-photo-2386144.webp" alt=""></img>`
+    c.innerHTML=`Tenha um bom dia`
+}
+
+
